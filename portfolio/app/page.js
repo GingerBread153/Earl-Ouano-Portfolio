d7f8246e50a74ac0c07b0ac0c1e227e75a08587d
GingerBread <75080001+GingerBread153@users.noreply.github.com>
2025-11-24 05:04:20 +0800

    Updated css and js

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -2,6 +2,7 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Work from "./components/Work";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <Header />
       <Navbar />
       <About />
-      <About />
+      <Work />
     </>
   );
 }
